ea6cd0122af8527bf9c49527b132b678043a0b2e
andrewbloese-00 <andrewbloese@gmail.com>
2024-07-10 13:16:23 -0700

    added bar graph visualizer + stars to scene

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import "./style.css"
-import { Vector3, Vector2, CatmullRomCurve3, TubeGeometry, EdgesGeometry, LineBasicMaterial, LineSegments, WebGLRenderer, Scene, PerspectiveCamera, FogExp2, ACESFilmicToneMapping, SRGBColorSpace } from "three"
+import { Vector3, Vector2, CatmullRomCurve3, TubeGeometry, EdgesGeometry, LineBasicMaterial, LineSegments, WebGLRenderer, Scene, PerspectiveCamera, FogExp2, ACESFilmicToneMapping, SRGBColorSpace, MathUtils, SphereGeometry, Mesh, MeshBasicMaterial, DoubleSide } from "three"
 import { RenderPass, UnrealBloomPass, EffectComposer } from "three/examples/jsm/Addons.js";
 
 const AudioContext = window.AudioContext || window.webkitAudioContext
@@ -8,7 +8,7 @@ const AudioContext = window.AudioContext || window.webkitAudioContext
 const TUBE_SEGMENTS = 222;
 const TUBE_RADIUS = 0.65;
 const RADIAL_SEGMENTS = 16;
-const LOOP_TIME = 10_000;
+const LOOP_TIME = 5_000;
 const HUE_ROTATE_AMOUNT = 1;
 const FFT_SIZE = 2048;
 
@@ -34,7 +34,6 @@ function getCircleTube(path_radius=100,path_segments=500, tube_radius=TUBE_RADIU
     points.push(new Vector3(path_radius*Math.cos(theta), path_radius*Math.sin(theta),0));
   }
   points.push(points[0])
-
   const curve = new CatmullRomCurve3(points,true);
   curve.closed = true
   const tubeGeometry = new TubeGeometry(curve,tube_segments,tube_radius,RADIAL_SEGMENTS,false)
@@ -55,7 +54,7 @@ function getVisualEnvironment(fogDensity=0.3){
     renderer.toneMapping = ACESFilmicToneMapping
     renderer.outputColorSpace = SRGBColorSpace
     document.body.appendChild(renderer.domElement)
-    
+    renderer.domElement.style.zIndex = "1"
     const scene = new Scene()
     //apply fog if params specified
     if(fogDensity > 0){
@@ -134,8 +133,12 @@ class AudioEnvironment {
   }
 
   getAverageLoudness(){
-    let sum = 0, activeCount = 1;
+    let sum = 0, activeCount = 1, bassSum;
+    const mid = this.analyser.frequencyBinCount/2
+
     for(let i = 0; i < this.analyser.frequencyBinCount; i++){
+      if(i === mid) bassSum = sum
+
       if(this.byteFrequencyData[i]){
         sum += this.byteFrequencyData[i]
         activeCount++
@@ -146,15 +149,71 @@ class AudioEnvironment {
     const average = sum / this.analyser.frequencyBinCount;
     const activeAverage = sum/activeCount;
     const adjustedAverage = (average+activeAverage/2)
+    const bassAvg =  bassSum / mid
+    const trebAvg = (sum-bassSum) / mid
     return {
-      average,activeAverage,adjustedAverage
+      average,activeAverage,adjustedAverage,
+      bassAvg, trebAvg
     }
   }
+
+  //gets a smaller array of averaged values for equal sized bins
+  getSimplifiedValues(binsArray){
+    const nBins = binsArray.length
+    const maxFFTIdx = Math.floor(0.7*this.analyser.frequencyBinCount)
+    const fftBinsPerBin =  Math.floor(maxFFTIdx / nBins)
+    let f = 0 //current fft idx
+    for(let i = 0; i < nBins;i++){
+      let sum = 0
+      for(let j = 0; j < fftBinsPerBin;j++){
+        sum += this.byteFrequencyData[f]
+        f++
+      }
+      binsArray[i] = (sum/fftBinsPerBin)/255
+    }
+
+  
+  }
+
 } 
 
 
 
 
+
+
+/**
+ * 
+ * @param {number} n 
+ * @param {Scene} scene 
+ */
+function addStars(n,scene){
+  const stars = []
+  for(let i = 0; i < n; i++){
+    const x = MathUtils.randFloat(-500,500)
+    const y = MathUtils.randFloat(-500,500)
+    const z = MathUtils.randFloat(-500,500)
+
+    const star = new Mesh(
+        new SphereGeometry(Math.random()),
+        new MeshBasicMaterial({
+          color: 0xffffff,
+          wireframe: true,
+          side: DoubleSide
+        })
+    )
+    star.position.set(x,y,z)
+    stars.push(star)
+    scene.add(star)
+  }
+  return stars
+
+
+
+
+
+
+}
 
 
 
@@ -163,12 +222,53 @@ async function initializeVisualizer(){
   if(!audioEnv) {
     return console.error("failed to get audio environment... cannot start visualizer");
   }
+
   const worldEnv = getVisualEnvironment(0);
-  
-  console.time("Generate Tube")
+  const stars = addStars(audioEnv.analyser.frequencyBinCount,worldEnv.scene)
   const tube = getCircleTube(100,500);
   worldEnv.scene.add(tube.visual);
-  console.timeEnd("Generate Tube")
+
+  /**
+   * @type {null|HTMLCanvasElement}
+   */
+  const canvas2d = document.querySelector("canvas#simpleViz")
+  canvas2d.width = 400
+  canvas2d.height = 200
+  const ctx2d = canvas2d.getContext("2d");
+  
+  
+  const COLORS = [ 
+    "#ff00ff",
+    "#ff10ff",
+    "#ff20ff",
+    "#ff30ff",
+    "#ff40ff",
+    "#ff50ff",
+    "#ff60ff",
+    "#ff70ff",
+    "#ff80ff",
+    "#ff90ff",
+
+    
+  ]
+
+
+  function drawBars(simple){
+    let c = 0; 
+    ctx2d.fillStyle = "#ffffff"
+    ctx2d.strokeStyle = "#ffffff"
+    const barWidth = 400/simple.length
+    for(let b = 0; b < simple.length; b++){
+      const x = b*barWidth
+      const height = (simple[b] * 250 )
+      ctx2d.fillStyle=COLORS[b]
+      ctx2d.fillRect(x,0,barWidth,height)
+
+    }
+  }
+
+
+
   
 
   function updateCamera(t){
@@ -185,6 +285,7 @@ async function initializeVisualizer(){
   }
   let t = 65; //"tube" time
   let ct = 0; //"color" time
+  const simple = Array(10).fill(0)
 
   function animate(){
     //render frame
@@ -194,14 +295,33 @@ async function initializeVisualizer(){
     //get current audio data
     audioEnv.analyser.getByteFrequencyData(audioEnv.byteFrequencyData);
     audioEnv.analyser.getByteTimeDomainData(audioEnv.timeDomainData);
-    const {average, adjustedAverage} = audioEnv.getAverageLoudness();
+    audioEnv.getSimplifiedValues(simple)
+    
+    
+    
+
+    const {adjustedAverage, trebAvg, bassAvg} = audioEnv.getAverageLoudness();
     worldEnv.bloomPass.strength = (adjustedAverage/255) * 150    
 
-    worldEnv.renderer.domElement.style.filter = `hue-rotate(${(ct+average)/(HUE_ROTATE_AMOUNT)}deg)`
+    const scaleAmount =  0.2*Math.sin((bassAvg +ct)/1000)
+    const s = 1 + scaleAmount
+    tube.visual.scale.set(1,1,s);
+
+
+    worldEnv.renderer.domElement.style.filter = `hue-rotate(${(ct+trebAvg)/(HUE_ROTATE_AMOUNT)}deg)`
+    canvas2d.style.filter = `hue-rotate(${(ct+trebAvg)/(HUE_ROTATE_AMOUNT)}deg) contrast(${Math.min(Math.abs(bassAvg-trebAvg),5)}) blur(15px)`
     t++;
     ct++
-    if(t > 1000 ) t  = 140
-  
+    if(t > 1000 ) t  = 144
+
+    for(const star of stars){
+      star.scale.set(trebAvg/255,(trebAvg+bassAvg)/2/255,trebAvg/255)
+    }
+    
+    ctx2d.clearRect(0,0,400,200) 
+    drawBars(simple);
+
+
     setTimeout(()=>{
       requestAnimationFrame(animate)
     },17) //~60fps
